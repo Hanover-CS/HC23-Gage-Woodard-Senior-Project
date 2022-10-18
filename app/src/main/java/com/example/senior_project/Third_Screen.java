c4c71287312b82a9e288a438b7d8bd24af55aa54
woodardg23 <woodardg23@hanover.edu>
2022-10-18 13:21:16 -0400

    Add Third_Screen.java class and xml for third screen

---
+++ app/src/main/java/com/example/senior_project/Third_Screen.java
@@ -0,0 +1,20 @@
+package com.example.senior_project;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Third_Screen extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+}
