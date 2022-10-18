9b434bb267dc45b429b1ea662de050db2fc5571c
woodardg23 <woodardg23@hanover.edu>
2022-10-18 13:22:07 -0400

    Add Second_Screen activity to manifest.

--- app/src/main/java/com/example/senior_project/Second_Screen.java
+++ app/src/main/java/com/example/senior_project/Second_Screen.java
@@ -1,15 +1,28 @@
 package com.example.senior_project;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Second_Screen extends AppCompatActivity {
+public class Second_Screen extends AppCompatActivity implements View.OnClickListener {
+
+    Button third_screen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        third_screen = findViewById(R.id.third_screen_button);
+        third_screen.setOnClickListener(this);
     }
 
+    @Override
+    public void onClick(View view) {
+        Intent intent = new Intent(Second_Screen.this, Third_Screen.class);
+        startActivity(intent);
+    }
 }
