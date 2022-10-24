3ecc1b3f07c30d8fe5ddf916ffca0351efba7d1a
woodardg23 <woodardg23@hanover.edu>
2022-10-24 14:01:46 -0400

    Add Second_Screen and Third_Screen to manifest. Add button options on activity_second.xml

--- app/src/main/java/com/example/senior_project/Second_Screen.java
+++ app/src/main/java/com/example/senior_project/Second_Screen.java
@@ -9,7 +9,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class Second_Screen extends AppCompatActivity implements View.OnClickListener {
 
-    Button third_screen;
+    Button third_screen, home_screen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +18,15 @@ public class Second_Screen extends AppCompatActivity implements View.OnClickList
 
         third_screen = findViewById(R.id.third_screen_button);
         third_screen.setOnClickListener(this);
+
+        home_screen = findViewById(R.id.home_screen_button);
+        home_screen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Second_Screen.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
