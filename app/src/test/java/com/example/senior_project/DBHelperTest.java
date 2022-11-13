06ccb8e8e6632ddce1e10dd36db4623d719eb0b4
woodardg23 <woodardg23@hanover.edu>
2022-11-12 20:01:54 -0500

    Add test files to start testing.

---
+++ app/src/test/java/com/example/senior_project/DBHelperTest.java
@@ -0,0 +1,19 @@
+package com.example.senior_project;
+
+import static org.junit.Assert.*;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import org.junit.Test;
+
+public class DBHelperTest {
+
+
+    @Test
+    public void testAddEntry() {
+
+
+    }
+
+}
---
+++ app/src/test/java/com/example/senior_project/MainActivityTest.java
@@ -0,0 +1,15 @@
+package com.example.senior_project;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import android.content.Intent;
+
+public class MainActivityTest {
+    
+    @Test
+    public void testButtonClick() {
+
+    }
+
+}
