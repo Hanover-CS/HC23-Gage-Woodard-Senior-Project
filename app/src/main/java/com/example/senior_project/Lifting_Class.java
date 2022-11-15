7acf5de7d85c8812c0a5b95ceba7f13c1d174977
woodardg23 <woodardg23@hanover.edu>
2022-11-15 13:32:15 -0500

    Refactor Lift_Class.java
    ref#9

--- app/src/main/java/com/example/senior_project/Lifting_Class.java
+++ app/src/main/java/com/example/senior_project/Lifting_Class.java
@@ -2,78 +2,38 @@ package com.example.senior_project;
 
 public class Lifting_Class {
 
-    private String day;
-    private String liftOne;
-    private String liftTwo;
-    private String liftThree;
-    private String liftFour;
-    private String liftFive;
-    private int id;
+    private String lift;
+    private int number_sets;
+    private int number_reps;
 
-    public String getDay() {
-        return day;
+    public String getLift() {
+        return lift;
     }
 
-    public void setDay(String day) {
-        this.day = day;
+    public void setLift(String lift) {
+        this.lift = lift;
     }
 
-    public String getLiftOne() {
-        return liftOne;
+    public int getNumber_sets() {
+        return number_sets;
     }
 
-    public void setLiftOne(String liftOne) {
-        this.liftOne = liftOne;
+    public void setNumber_sets(int sets) {
+        this.number_sets = sets;
     }
 
-    public String getLiftTwo() {
-        return liftTwo;
+    public int getNumber_reps() {
+        return number_reps;
     }
 
-    public void setLiftTwo(String liftTwo) {
-        this.liftTwo = liftTwo;
+    public void setNumber_reps(int reps) {
+        this.number_reps = reps;
     }
 
-    public String getLiftThree() {
-        return liftThree;
-    }
-
-    public void setLiftThree(String liftThree) {
-        this.liftThree = liftThree;
-    }
-
-    public String getLiftFour() {
-        return liftFour;
-    }
-
-    public void setLiftFour(String liftFour) {
-        this.liftFour = liftFour;
-    }
-
-    public String getLiftFive() {
-        return liftFive;
-    }
-
-    public void setLiftFive(String liftFive) {
-        this.liftFive = liftFive;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public Lifting_Class(String day, String liftOne, String liftTwo, String liftThree,
-                         String liftFour, String liftFive) {
-        this.day = day;
-        this.liftOne = liftOne;
-        this.liftTwo = liftTwo;
-        this.liftThree = liftThree;
-        this.liftFour = liftFour;
-        this.liftFive = liftFive;
+    public Lifting_Class(String lift, int sets, int reps) {
+        this.lift = lift;
+        this.number_sets = sets;
+        this.number_reps = reps;
     }
 
 }
