01ab870823068c0d1aa8f74ed5dea7e6be6fcb52
woodardg23 <woodardg23@hanover.edu>
2023-02-06 22:49:37 -0500

    Test workout DB.

---
+++ app/src/test/java/com/example/senior_project/DataBaseHelperTest.java
@@ -0,0 +1,30 @@
+package com.example.senior_project;
+
+import android.content.Context;
+
+import androidx.room.Room;
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import static org.junit.Assert.*;
+
+public class DataBaseHelperTest {
+
+    private DBHelper db;
+    private Workout workout;
+
+    @Test
+    public void addNewLiftTest() {
+
+    }
+
+    @Test
+    public void deleteLiftTest() {
+
+    }
+
+}
